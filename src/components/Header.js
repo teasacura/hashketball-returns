388008c078d27619071907452aa55344342277c4
teasacura <36260380+teasacura@users.noreply.github.com>
2018-10-26 13:38:32 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Header } from 'semantic-ui-react'
 
 const NavHeader = () => {
   return (
-    <Header className='header' as='h5' block>
+    <Header className='header' block>
       <h3> Hashketball: The Dribbling </h3>
       <img className='App-logo' atl='basketball' src='https://images.vexels.com/media/users/3/135385/isolated/lists/212a5985af3fc8329ada7bc2a45bad82-basketball-circle-icon.png'></img>
     </Header>
